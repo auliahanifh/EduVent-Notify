42160fbec6d3b2c0011e2e416455fb9f8fc66da3
auliahanifh <namedauliah@gmail.com>
2026-07-21 09:22:08 +0700

    .

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ print("Jalankan server EduVent...")
 
 while True:
     print("Cek update tugas terbaru di EduVent...")
-    os.system("python -u mailsend.py")
     os.system("python -u wasend.py")
+    os.system("python -u mailsend.py")
     print("Jeda 3 menit sebelum pengecekan selanjutnya...\n")
     time.sleep(180)
